10fee7b4e1e4ced6e2a72d52291f8d9ad838ac1f
DilettaZecchinetti <155912808+DilettaZecchinetti@users.noreply.github.com>
2024-11-26 10:14:47 +0000

    Revert "Task 3"

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,50 +8,16 @@ const app = require("../app");
 const db = require("../db/connection");
 
 /* Set up your beforeEach & afterAll functions here */
-beforeEach(() => {
-  return seed(testData);
-});
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
 
-afterAll(() => {
-  return db.end();
-});
-
-describe("GET /api", () => {
+describe.only("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
-      });
-  });
-
-  describe("GET /api/topics", () => {
-    test("200: Responds with an array of all topics", () => {
-      return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({ body }) => {
-          const { topics } = body;
-          expect(topics.length).toBeGreaterThan(0);
-          topics.forEach((topic) => {
-            expect(topic).toEqual(
-              expect.objectContaining({
-                slug: expect.any(String),
-                description: expect.any(String),
-              })
-            );
-          });
-        });
-    });
-  });
-
-  test("404: Responds with an error when the endpoint does not exist", () => {
-    return request(app)
-      .get("/api/non-existent-endpoint")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Endpoint not found");
       });
   });
 });
--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 
 const { getEndpoints } = require("./db/controllers/api.controller.js");
-const { getTopics } = require("./db/controllers/topics.controller.js");
 
 app.get("/api", getEndpoints);
 
-app.get("/api/topics", getTopics);
-
-app.use("/*", (req, res) => {
-  res.status(404).send({ msg: "Endpoint not found" });
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
 });
 
 module.exports = app;

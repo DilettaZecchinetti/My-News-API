fafbc8dc6a75dd0af1937a2e2fd4beb3f40c2373
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-11-26 15:42:31 +0000

    task 4 complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,13 +11,51 @@ const db = require("../db/connection");
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe.only("GET /api", () => {
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an article object each with the correct properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toMatchObject({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: expect.any(Number),
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+        });
+      });
+  });
+
+  test("404: responds with error as article does not exist", () => {
+    return request(app)
+      .get("/api/articles/789")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Id not found");
+      });
+  });
+
+  test("400: responds with error as it has been given wrong id", () => {
+    return request(app)
+      .get("/api/articles/NaN")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("bad request");
       });
   });
 });
--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const app = express();
 
 const { getEndpoints } = require("./db/controllers/api.controller.js");
 
+const {
+  getArticleById,
+} = require("./db/controllers/articlesbyid.controller.js");
+
 app.get("/api", getEndpoints);
 
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use("/*", (req, res) => {
+  res.status(404).send({ msg: "Endpoint not found" });
+});
+
 app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    res.status(500).send({ msg: "Internal Server Error" });
-  }
+  const { status = 500, msg = "Internal Server Error" } = err;
+
+  res.status(status).send({ msg });
 });
 
 module.exports = app;

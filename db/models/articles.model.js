051e0112e67d7ed20b73e3ac0a93d5534cd982d5
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-11-30 11:55:56 +0000

    task 8 complete

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -26,3 +26,22 @@ exports.fetchAllArticles = () => {
       return rows;
     });
 };
+
+///////
+
+exports.updateArticleByArticleId = (article_id, inc_votes) => {
+  return db
+    .query(
+      `UPDATE articles 
+     SET votes = votes + $1 
+     WHERE article_id = $2 
+     RETURNING *`,
+      [inc_votes, article_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    });
+};

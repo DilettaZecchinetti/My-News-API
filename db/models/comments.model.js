b6a3ed61206c26da55cfbe991523596b1cbb3bcf
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-12-12 18:27:23 +0000

    modified content

--- db/models/comments.model.js
+++ db/models/comments.model.js
@@ -4,7 +4,7 @@ const fetchCommentsByArticleId = (article_id) => {
   const queryStr = `SELECT comment_id, body, article_id, author, votes, created_at 
        FROM comments 
        WHERE article_id= $1 
-       ORDER by created_at DESC`;
+       ORDER by created_at DESC;`;
 
   return db.query(queryStr, [article_id]).then(({ rows }) => {
     return rows;
